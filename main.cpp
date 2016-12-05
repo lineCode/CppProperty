55a53831640ee653634cb2fd6579088dd7ce3102
Srivathsan Madhavan <m.srivathsan@gmail.com>
2016-12-05 17:18:38 +0530

    Interim Commit

--- main.cpp
+++ main.cpp
@@ -14,31 +14,140 @@
 #include <iostream>
 #include "property.h"
 
-class Dummy {
-private:
-    int m_var;
+#define VERBOSE(x, f, l) x; std::cout << f << ":" << l << " --> "  \
+                            << #x << std::endl;
+#define PRINT(x) #x << "==" << x
+#define PRINT_HDR(x) std::cout << std::endl << "==== " << x << " ====" << std::endl;
 
-public:
-    Dummy(const int& val) : m_var(val), Value(m_var), ConstValue(m_var) {
-        autoValue = 999;
+void SimplestUseCase() {
+    PRINT_HDR(__FUNCTION__);
+    
+    class PropertyWrapper {
+    private:
+        int m_var;
+
+    public:
+        PropertyWrapper(const int& val) : m_var(val), property(m_var) {};
+
+        Property<int> property;
+        
+        const int getVal() const { return m_var; }
+        void setVal(int val) { m_var = val; }
     };
 
-    Property<int> autoValue;
-    Property<int> Value;
-    const Property<int> ConstValue;
-    int getVal() const { return m_var; }
-};
+    VERBOSE(PropertyWrapper obj(100);, __FILE__, __LINE__);
+    std::cout << PRINT(obj.property)
+            << "; " << PRINT(obj.getVal()) << std::endl;
+    VERBOSE(obj.property = 200;, __FILE__, __LINE__);
+    std::cout << PRINT(obj.property)
+            << "; " << PRINT(obj.getVal()) << std::endl;
+    VERBOSE(obj.setVal(300);, __FILE__, __LINE__);
+    std::cout << PRINT(obj.property)
+            << "; " << PRINT(obj.getVal()) << std::endl;
+}
+
+void PropertyInConstObject() {
+    PRINT_HDR(__FUNCTION__);
+    
+    class PropertyWrapper {
+    private:
+        int m_var;
+
+    public:
+        PropertyWrapper(const int& val) : m_var(val), property(m_var) {};
+
+        Property<int> property;
+        
+        const int getVal() const { return m_var; }
+        void setVal(int val) { m_var = val; }
+    };
+
+    VERBOSE(const PropertyWrapper obj(100);, __FILE__, __LINE__);
+    std::cout << PRINT(obj.property)
+            << "; " << PRINT(obj.getVal()) << std::endl;
+    // obj.property = 200; - compile error
+    // obj.setVal(300); - compile error
+}
+
+void ExplicitReadonlyProperty() {
+    PRINT_HDR(__FUNCTION__);
+    
+    class PropertyWrapper {
+    private:
+        int m_var;
+
+    public:
+        PropertyWrapper(const int& val) : m_var(val), property(m_var) {};
+
+        const Property<int> property;
+        
+        const int getVal() const { return m_var; }
+        void setVal(int val) { m_var = val; }
+    };
+
+    VERBOSE(PropertyWrapper obj(100);, __FILE__, __LINE__);
+    std::cout << PRINT(obj.property)
+            << "; " << PRINT(obj.getVal()) << std::endl;
+
+    // obj.property = 200; - compile error
+    VERBOSE(obj.setVal(200);, __FILE__, __LINE__);
+    std::cout << PRINT(obj.property)
+            << "; " << PRINT(obj.getVal()) << std::endl;
+}
+
+void AutoProperties() {
+    PRINT_HDR(__FUNCTION__);
+    
+    std::cout << "---- typical use ----" << std::endl;
+    VERBOSE(Property<int> prop;, __FILE__, __LINE__);
+    VERBOSE(prop = 1234;, __FILE__, __LINE__);
+    std::cout << PRINT(prop) << std::endl << std::endl;
+    
+    std::cout << "---- Use of Move Semantics with Auto-Property ----" << std::endl;
+    VERBOSE(Property<int> prop2;, __FILE__, __LINE__);
+    VERBOSE(prop2 = 9999;, __FILE__, __LINE__);
+    VERBOSE(auto prop3 = std::move(prop2);, __FILE__, __LINE__);
+    std::cout << PRINT(prop3) << std::endl << std::endl;
+    
+    try {
+        std::cout << "---- Try using the 'moved-out' property ----" << std::endl;
+        std::cout << __FILE__ << ":" << __LINE__ << " prop2==";
+        std::cout << prop2 << std::endl;
+    } catch(const std::exception &e) {
+        std::cout << " >>>> EXCEPTION occurred; " << e.what() << ". "
+                << __FILE__ << ":" << __LINE__
+                << " <<<<" << std::endl;
+    } catch(...) {
+        std::cout << " >>>> UNKNOWN EXCEPTION occurred; "
+                << __FILE__ << ":" << __LINE__
+                << " <<<<" << std::endl;
+    }
+    std::cout << std::endl;
+
+    try {
+        std::cout << "---- Typical Referencing / Wrapping Property ----" << std::endl;
+        VERBOSE(int dummy = 6666;, __FILE__, __LINE__);
+        VERBOSE(Property<int> prop4(dummy);, __FILE__, __LINE__);
+        std::cout << PRINT(prop4) << std::endl << std::endl;
+        
+        std::cout << "---- Try moving out the above Property ----" << std::endl;
+        VERBOSE(auto prop5 = std::move(prop4);, __FILE__, __LINE__);
+    } catch(const std::exception &e) {
+        std::cout << " >>>> EXCEPTION occurred; " << e.what() << ". "
+                << __FILE__ << ":" << __LINE__
+                << " <<<<" << std::endl;
+    } catch(...) {
+        std::cout << " >>>> UNKNOWN EXCEPTION occurred; "
+                << __FILE__ << ":" << __LINE__
+                << " <<<<" << std::endl;
+    }
+}
 
 int main(int argc, char** argv) {
-    Dummy dummy(100);
-    std::cout << dummy.Value << " " << dummy.getVal() << std::endl;
-
-    dummy.Value = 200;
-    std::cout << dummy.ConstValue << " " << dummy.getVal() << std::endl;
-    
-    std::cout << dummy.autoValue << " " << std::endl;
-    dummy.autoValue = 666;
-    std::cout << dummy.autoValue << " " << std::endl;
+//    SimplestUseCase();
+//    PropertyInConstObject();
+//    ExplicitReadonlyProperty();
+    AutoProperties();
     return 0;
 }
 
--- property.h
+++ property.h
@@ -16,6 +16,7 @@
 
 #include <functional>
 #include <utility>
+#include <stdexcept>
 
 template <typename T> // T is a trivially copy-able/assignable/move-able type
 class Property {
@@ -23,35 +24,61 @@ public:
     typedef std::function<void (const T&)> Setter;
     typedef std::function<const T ()> Getter;
 
-    Property(const Property<T>& prop) : m_ref(m_val), m_val(prop.m_val) {
+    //---- Constructors ----
+    Property(const Property<T>& prop) noexcept(false) // copy-CTOR
+        : m_ref(m_val), m_val(prop.m_val)
+    {
         if (&prop.m_ref != &prop.m_val) {
-            throw "Only auto-properties can be copy-constructed";
+            throw std::logic_error("Only auto-properties can be copy-constructed");
         }
+        Set = prop.Set; Get = prop.Get;
     }
-    Property(Property<T>&& prop) : m_ref(m_val), m_val(std::move(prop.m_val)) {
+    Property(Property<T>&& prop) noexcept(false) // move-CTOR
+        : m_ref(m_val), m_val(std::move(prop.m_val))
+    {
         if (&prop.m_ref != &prop.m_val) {
-            throw "Only auto-properties can be move-constructed";
+            throw std::logic_error("Only auto-properties can be copy-constructed");
         }
+        Set = std::move(prop.Set); Get = std::move(prop.Get);
     }
     Property(
             T& var, Setter onSet = nullptr, Getter onGet = nullptr
-        ) : m_ref(var)
+        ) noexcept : m_ref(var)
     {
         InitHandlers(onSet, onGet);
     };
     Property(
             const T& var, Setter onSet = nullptr, Getter onGet = nullptr
-        ) : m_ref(var)
+        ) noexcept : m_ref(var)
     {
         InitHandlers(onSet, onGet);
     };
-    Property(Setter onSet = nullptr) : m_ref(m_val) { // ctor for auto-property
+    Property(Setter onSet = nullptr) noexcept // CTOR for auto-property
+        : m_ref(m_val)
+    {
         Getter onGet = nullptr;
         InitHandlers(onSet, onGet);
     }
     
-    const T operator=(const T& val) { Set(val); }
-    operator const T() const        { return Get(); }
+    //---- Assignment operator-overload ----
+    Property<T>& operator=(const Property<T>& prop) noexcept(false) {
+        if (&prop.m_ref != &prop.m_val && &m_ref != &m_val) {
+            throw std::logic_error("Only auto-properties are assignable");
+        }
+        m_val = prop.m_val;
+        return *this;
+    }
+    Property<T>& operator=(Property<T>&& prop) noexcept(false) {
+        if (&prop.m_ref != &prop.m_val && &m_ref != &m_val) {
+            throw std::logic_error("Only auto-properties are move-assignable");
+        }
+        m_val = std::move(prop.m_val);
+        return *this;
+    }
+    Property<T>& operator=(const T& val) { Set(val); return *this; }
+    
+    //---- typecast overload ----
+    operator const T() const { return Get(); }
     
 private:
     mutable Setter Set;
